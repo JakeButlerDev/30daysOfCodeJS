e562ccfe4ba602394de015653dfbe7684ba0fe2e
Jake Butler <JakeButler.student@careerdevs.com>
2022-06-09 18:24:27 -0400

    First commit

---
+++ day1/helloWorld.js
@@ -0,0 +1,7 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    var myVar = inputString
+    // Write the second line of output that prints the contents of 'inputString' here.
+   console.log(myVar)
+}
